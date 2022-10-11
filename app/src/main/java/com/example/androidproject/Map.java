d25ece4fa37194a3cf2e2854cbc6e2b088a4850e
JacksonMars <jmarsnas@my.bcit.ca>
2022-10-11 12:21:12 -0700

    Get started on map screen

---
+++ app/src/main/java/com/example/androidproject/Map.java
@@ -0,0 +1,13 @@
+package com.example.androidproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Map extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.map_layout);
+    }
+}
