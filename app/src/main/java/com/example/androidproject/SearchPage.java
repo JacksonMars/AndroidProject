8032ed32b8c33b293639bad275c1ccac8dae082d
CALEB VERMA <halasmish@gmail.com>
2022-10-13 09:59:11 -0700

    remove unused imports

--- app/src/main/java/com/example/androidproject/SearchPage.java
+++ app/src/main/java/com/example/androidproject/SearchPage.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
