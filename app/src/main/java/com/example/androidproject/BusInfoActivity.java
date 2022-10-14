1b0cbbf04aed50fa960ffdb5ff41365745faa127
CALEB VERMA <halasmish@gmail.com>
2022-10-14 11:54:47 -0700

    Add schedule fragment

--- app/src/main/java/com/example/androidproject/BusInfoActivity.java
+++ app/src/main/java/com/example/androidproject/BusInfoActivity.java
@@ -1,7 +1,9 @@
 package com.example.androidproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -14,6 +16,17 @@ public class BusInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bus_info_activity);
+
+        // Get the intent
+        Intent intentPrev = getIntent();
+
+        // Extract data from intent
+        Bundle bundlePrev = intentPrev.getBundleExtra("bundle");
+        String route = bundlePrev.getString("route");
+
+        //Set screen header as selected route
+        TextView textView = findViewById(R.id.title);
+        textView.setText(route);
 
         Fragment busInfoFragment = new BusInfoFragment();
 
