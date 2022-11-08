ebde2cbcddcc03f4541ed0fac35f11854a743a9f
JacksonMars <jmarsnas@my.bcit.ca>
2022-11-08 14:15:29 -0800

    Add route tracing to map fragment

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.androidproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,8 +34,16 @@ public class MainActivity extends AppCompatActivity {
         // Set bottom navbar
         BottomNavigationView bottomNavigationView= findViewById(R.id.bottom_navigation);
 
+        String tripId = getTripId("30039");
+        ArrayList<String> stopIds = getStopIds(tripId);
+        ArrayList<String> allCoordinates = allStopCoordinates(stopIds);
+
         // Set map fragment as default in frame layout
+        Bundle newBundle = new Bundle();
+        newBundle.putStringArrayList("coordinates", allCoordinates);
+
         Fragment mapFragment = new MapFragment();
+        mapFragment.setArguments(newBundle);
         getSupportFragmentManager()
                 .beginTransaction().replace(R.id.fragment_container, mapFragment)
                 .commit();
@@ -53,5 +69,93 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         });
+    }
+
+    public String getTripId(String intendedRouteId) {
+        String tripId =  null;
+        try {
+            InputStream inputStream = getBaseContext().getResources().openRawResource(R.raw.trips);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            bufferedReader.readLine();
+            String line = bufferedReader.readLine();
+
+            while(line != null && tripId == null) {
+                String[] tokenize = line.split(",");
+                String routeId = tokenize[0];
+                if(routeId.equals(intendedRouteId)) {
+                    tripId = tokenize[2];
+                }
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return tripId;
+    }
+
+    public ArrayList<String> getStopIds(String intendedTripId) {
+        ArrayList<String> allStops = new ArrayList<>();
+        try {
+            InputStream inputStream = getBaseContext().getResources().openRawResource(R.raw.stop_times);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            bufferedReader.readLine();
+            String line = bufferedReader.readLine();
+
+            while(line != null) {
+                String[] tokenize = line.split(",");
+                String routeId = tokenize[0];
+                if(routeId.equals(intendedTripId)) {
+                    allStops.add(tokenize[1] + "," + tokenize[3]);
+                }
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        allStops.sort(Comparator.naturalOrder());
+
+        return allStops;
+    }
+
+    public String getStopCoordinates(String intendedStopId) {
+        String coordinates = null;
+        try {
+            InputStream inputStream = getBaseContext().getResources().openRawResource(R.raw.stops);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            bufferedReader.readLine();
+            String line = bufferedReader.readLine();
+
+            while(line != null && coordinates == null) {
+                String[] tokenize = line.split(",");
+                String stopId = tokenize[0];
+                if(stopId.equals(intendedStopId)) {
+                    coordinates = tokenize[4] + "," + tokenize[5];
+                }
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return coordinates;
+    }
+
+    public ArrayList<String> allStopCoordinates(ArrayList<String> stopIds) {
+        ArrayList<String> allCoordinates = new ArrayList<>();
+        for(int i = 0; i < stopIds.size(); i++) {
+            allCoordinates.add(getStopCoordinates(stopIds.get(i).split(",")[1]));
+        }
+        return allCoordinates;
     }
 }
--- app/src/main/java/com/example/androidproject/MapFragment.java
+++ app/src/main/java/com/example/androidproject/MapFragment.java
@@ -1,10 +1,12 @@
 package com.example.androidproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,14 +17,37 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+import com.google.maps.model.TravelMode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+    private ArrayList<String> stopCoordinates;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Bundle bundle = this.getArguments();
+        stopCoordinates = bundle.getStringArrayList("coordinates");
+
         // Initialize view
         View view=inflater.inflate(R.layout.map_fragment, container, false);
 
@@ -40,8 +65,75 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        LatLng comp3717Lecture = new LatLng(49.25010954461797, -123.00275621174804);
-        googleMap.addMarker(new MarkerOptions().position(comp3717Lecture).title("COMP 3717 Lecture"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(comp3717Lecture, 15));
+        mMap = googleMap;
+        LatLng start = null;
+        List<LatLng> path = new ArrayList<>();
+
+        for(int i = 0; i < stopCoordinates.size() - 1; i++) {
+            String startCoords = stopCoordinates.get(i);
+            String endCoords = stopCoordinates.get(i + 1);
+            if(start == null) {
+                String[] tokenized = startCoords.split(",");
+                start = new LatLng(Float.parseFloat(tokenized[0]), Float.parseFloat(tokenized[1]));
+            }
+            addToRoute(startCoords, endCoords, path);
+        }
+
+        if(path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            mMap.addPolyline(opts);
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start, 13));
+    }
+
+    private void addToRoute(String location1, String location2, List<LatLng> path) {
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyA7B8cf57CuID9r8WHJCkmWa4P-cCGQNMo")
+                .build();
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, location1, location2).mode(TravelMode.TRANSIT);
+
+        try {
+            DirectionsResult res = req.await();
+
+            if(res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if(route.legs != null) {
+                    for(int i = 0; i < route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if(leg.steps != null) {
+                            for(int j = 0; j < leg.steps.length; j++) {
+                                DirectionsStep step = leg.steps[j];
+
+                                if(step.steps != null && step.steps.length > 0) {
+                                    for(int k = 0; k < step.steps.length; k++) {
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+
+                                        if(points1 != null) {
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for(com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if(points != null) {
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for(com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch(Exception e) {
+            Log.i("ERROR", e.getLocalizedMessage());
+        }
     }
 }
