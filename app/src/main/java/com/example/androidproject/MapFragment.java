91ac32b606349a2cb0024bcb272e1686a50f4ced
FijiMan24 <halasmish@gmail.com>
2022-11-19 19:41:35 -0800

    Add icon for active bus

--- app/src/main/java/com/example/androidproject/MapFragment.java
+++ app/src/main/java/com/example/androidproject/MapFragment.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -158,7 +159,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             LatLng busCoordinates = new LatLng(latitude, longitude);
             googleMap.addMarker(new MarkerOptions()
                     .position(busCoordinates)
-                    .title("Bus No. " + busNum));
+                    .title("Bus No. " + busNum)
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus_clipart)));
         }
     }
 }
